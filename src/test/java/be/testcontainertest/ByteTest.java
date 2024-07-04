546633a70278da9f6b6554a8aba6ae159ab8132b
Jens Schauder <jens@schauderhaft.de>
2024-07-04 14:22:07 +0200

    Added test operating directly on NamedParameterJdbcTemplate.

--- src/test/java/be/testcontainertest/ByteTest.java
+++ src/test/java/be/testcontainertest/ByteTest.java
@@ -5,44 +5,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.*;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @EnableJdbcRepositories(considerNestedRepositories = true)
 @ContextConfiguration(classes = {
-        SomeTableRepository.class
+		SomeTableRepository.class
 })
 @Sql(executionPhase = BEFORE_TEST_METHOD, value = {
-        "classpath:0_init.sql",
-        "classpath:1_load.sql"
+		"classpath:0_init.sql",
+		"classpath:1_load.sql"
 })
 @Sql(executionPhase = AFTER_TEST_METHOD, value = "classpath:2_clean.sql")
 class ByteTest extends MssqlContainerBaseTest {
 
-    @Autowired
-    private SomeTableRepository someTableRepository;
+	@Autowired
+	NamedParameterJdbcOperations template;
 
-    @Test
-    void testByteArrayNotNull() {
-        final SomeTable record = new SomeTable(null, "abc".getBytes());
-        someTableRepository.save(record);
-    }
+	@Autowired
+	private SomeTableRepository someTableRepository;
 
-    @Test
-    void testByteArrayNull() {
-        final SomeTable record = new SomeTable(null, null);
-        someTableRepository.save(record);
-    }
+	@Test
+	void testByteArrayNotNull() {
+		final SomeTable record = new SomeTable(null, "abc".getBytes());
+		someTableRepository.save(record);
+	}
 
-    @Test
-    void testByteArrayEmpty() {
-        final SomeTable record = new SomeTable(null, new byte[]{});
-        someTableRepository.save(record);
-    }
+	@Test
+	void testByteArrayNull() {
+		final SomeTable record = new SomeTable(null, null);
+		someTableRepository.save(record);
+	}
+
+	@Test
+	void testByteArrayEmpty() {
+		final SomeTable record = new SomeTable(null, new byte[]{});
+		someTableRepository.save(record);
+	}
+
+	@Test
+	void templateNullArray() {
+		template.update(
+				"INSERT INTO \"SOME_TABLE\" (\"CONTENT\") VALUES (:CONTENT)",
+				new MapSqlParameterSource("CONTENT", null)
+		);
+	}
 
 }
